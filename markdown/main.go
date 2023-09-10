9ed9e6ceff38e5b3740027f18e1bdea46a2df1c6
Omkar Bhagat <omkarbhagat93+github@gmail.com>
2023-09-10 23:09:14 +0100

    markdown with headers and paragraphs

--- markdown/main.go
+++ markdown/main.go
@@ -1,32 +1,73 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"os"
+	"path/filepath"
 	"regexp"
 	"strings"
 )
 
-func main() {
-	inputText := `
-# This is a Header
-This is a paragraph of text.
+var (
+	markDownFile string
+	inParagraph  bool
+	result       bytes.Buffer
+)
 
-# Another Header
-Here's another paragraph.
-`
+func setMarkDownFilePath() {
+	p, err := filepath.Abs("./main.go")
+	if err != nil {
+		fmt.Println("Error reading filepath: ", err)
+		return
+	}
+	markDownFile = filepath.Dir(p) + "/text.md"
+}
+
+func maybeCloseParagraph() {
+	if inParagraph {
+		inParagraph = false
+		result.WriteString("</p>")
+	}
+}
+
+func textToHtml(text string) string {
 
 	// Using the blackfriday package
 	// html := textToHTML(inputText)
 	// html := string(blackfriday.Run([]byte(inputText)))
 	// fmt.Println(html)
 
+	inParagraph = false
+
 	headerRegex := regexp.MustCompile(`^#\s+(.*)$`)
-	lines := strings.Split(inputText, "\n")
+	paragraphRegex := regexp.MustCompile(`^(.*)$`)
+
+	lines := strings.Split(text, "\n")
 	for _, line := range lines {
 		if matches := headerRegex.FindStringSubmatch(line); matches != nil {
-			fmt.Printf("%v", matches[1])
+			maybeCloseParagraph()
+			result.WriteString("<h1>")
+			result.WriteString(matches[1])
+			result.WriteString("</h1>")
+		} else if matches := paragraphRegex.FindStringSubmatch(line); matches != nil {
+			inParagraph = true
+			result.WriteString("<p>")
+			result.WriteString(matches[1])
 		}
 	}
 
-	fmt.Println()
+	maybeCloseParagraph()
+	return result.String()
+}
+
+func main() {
+	setMarkDownFilePath()
+	text, err := os.ReadFile(markDownFile)
+	if err != nil {
+		fmt.Println("Error reading file: ", err)
+		return
+	}
+	fmt.Println(textToHtml(string(text)))
+	// os.WriteFile("result.html", []byte(result.String()), 0644)
 }
---
+++ markdown/text.md
@@ -0,0 +1,9 @@
+# This is a Header
+This is a paragraph of text.
+This is a paragraph of text.
+
+This is a paragraph of text after space.
+This is a paragraph of text.
+
+# Another Header
+Here's another paragraph.
