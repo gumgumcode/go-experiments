63dc15f393f1a0a3729e0b523b63e1a0c2d2317e
Omkar Bhagat <omkarbhagat93+github@gmail.com>
2023-09-19 20:37:06 +0100

    more hangman

--- hangman/main.go
+++ hangman/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"math/rand"
 	"slices"
+	"strings"
 )
 
 var words = []string{
@@ -31,10 +32,51 @@ func displayWord(word string, guessedLetters []string) string {
 func main() {
 	randomIndex := rand.Intn(len(words))
 	chosenWord := words[randomIndex]
-	guessedLetters := []string{"a"}
+	guessedLetters := []string{}
+
+	maxIncorrectGuesses := 6
+	incorrectGuesses := 0
 
 	for {
-		fmt.Println("\n Word: ", displayWord(chosenWord, guessedLetters))
-		break
+		// Display the current state of the word
+		fmt.Println("\nWord:", displayWord(chosenWord, guessedLetters))
+
+		// Prompt the player for a letter guess
+		fmt.Print("Guess a letter: ")
+		var guess string
+		fmt.Scanln(&guess)
+
+		// Validate the guess (ensure it's a single letter)
+		if len(guess) != 1 || !isLetter(guess) {
+			fmt.Println("Invalid input. Please enter a single letter.")
+			continue
+		}
+
+		// Check if the guess is correct
+		if strings.Contains(chosenWord, guess) {
+			// Update the guessedLetters slice with the correct guess
+			guessedLetters = append(guessedLetters, guess)
+			// Check if the player has won
+			if strings.EqualFold(chosenWord, displayWord(chosenWord, guessedLetters)) {
+				fmt.Println("Congratulations! You've won. The word was:", chosenWord)
+				break
+			}
+		} else {
+			fmt.Println("Incorrect guess.")
+			incorrectGuesses++
+			// Check if the player has lost
+			if incorrectGuesses >= maxIncorrectGuesses {
+				fmt.Println("Sorry, you've run out of guesses. The word was:", chosenWord)
+				break
+			}
+		}
 	}
+	fmt.Println("\nThank you for playing Hangman!")
+}
+
+func isLetter(s string) bool {
+	if len(s) != 1 {
+		return false
+	}
+	return ('a' <= s[0] && s[0] <= 'z') || ('A' <= s[0] && s[0] <= 'Z')
 }
